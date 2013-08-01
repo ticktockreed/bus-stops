2a5f6c150a5b37f17211c0ad7575fba60460a9f9
Tim Reed <treed@thisiszone.com>
2013-08-01 11:28:23 +0100

    add scripts

---
+++ scripts/README.md
@@ -0,0 +1,21 @@
+Scripts folder
+<!--
+|-- app
+|   |-- controllers
+|   |   |-- admin
+|   |   |   |-- postsController.js
+|   |   |   -- usersController.js
+|   |   |-- postsController.js
+|   |   |-- sessionsController.js
+|   |   -- usersController.js
+|   |-- models
+|   |   |-- post.js
+|   |   -- user.js
+|   |-- views
+|   |   |-- admin
+|   |   |   -- posts
+|   |   |       |-- edit.jade
+|   |   |       |-- index.jade
+|   |   |       |-- new.jade
+|   |   |-- layouts
+|   |   |   -- application.jade
---
+++ scripts/app.js
@@ -0,0 +1,52 @@
+
+var app;
+
+(function ($) {
+  "use strict";
+
+
+  app = {
+
+      /* CONSTANTS
+  ----------------------------------------------------------------------------- */
+
+      CONSTANTS: {
+        // Place sitewide global variables here
+      },
+
+      animation : {
+        // Place common animation variables here - eg speed, easing etc.
+      },
+
+      /* FUNCTIONS
+ 
+  ----------------------------------------------------------------------------- */
+
+    /*
+    *   init
+    *   ---------------------------------------------
+    *   Start scripts
+    *   ---------------------------------------------
+    *   @param n/a
+    *   @return Void
+    */
+    
+    init: function() init {
+
+      app.first();
+
+    },
+
+    firstFunction: function() firstFunction {
+
+    }
+
+  };
+
+
+  $(function() {
+
+      app.init();
+
+  });
+})(jQuery);
